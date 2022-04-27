5c17397128e17028dc3b9c4e8eb575f2f38cd53c
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-27 21:44:12 +0200

    implemented component info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Actions from "./components/Actions/Actions";
+import Info from "./components/Info/Info";
 import Keyboard from "./components/Keyboard/Keyboard";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        {/* <!-- El siguiente elemento se oculta añadiéndole la clase "off" --> */}
-        <span className="message">Calling...</span>
+        <Info infoTex={"Calling..."} />
         <main className="phone">
           <div className="keyboard-container">
             <Keyboard />
---
+++ src/components/Info/Info.js
@@ -0,0 +1,5 @@
+const Info = ({ infoTex }) => {
+  return <span className="message">{infoTex}</span>;
+};
+
+export default Info;
