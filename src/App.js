4f44a528b1fbfc44484929612878aae81eb9045c
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-27 22:25:48 +0200

    added basic context provider dunctionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useContext } from "react";
 import Actions from "./components/Actions/Actions";
 import Info from "./components/Info/Info";
 import Keyboard from "./components/Keyboard/Keyboard";
+import ActionContext from "./context/ActionContext";
 
 function App() {
+  const textTest = useContext(ActionContext);
   return (
     <div className="App">
       <div className="container">
-        <Info infoTex={"Calling..."} />
+        <Info infoTex={textTest} />
         <main className="phone">
           <div className="keyboard-container">
             <Keyboard />
@@ -17,5 +20,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
