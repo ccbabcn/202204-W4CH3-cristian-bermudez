805b395be78f5676c29059508674e99b404955bc
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-27 21:20:20 +0200

    implmeneted basic action component

---
+++ src/components/Action/Action.js
@@ -0,0 +1,14 @@
+const Action = () => {
+  return (
+    <>
+      <a href="?#" className="call">
+        Call
+      </a>
+      <a href="?#" className="hang">
+        Hall
+      </a>
+    </>
+  );
+};
+
+export default Action;
