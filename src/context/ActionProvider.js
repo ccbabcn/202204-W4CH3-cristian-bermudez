09a020169165e03e22f53149d649b954f8ecc6f4
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-28 07:57:12 +0200

    implemented function keyhandler on keyboard component

--- src/context/ActionProvider.js
+++ src/context/ActionProvider.js
@@ -3,6 +3,7 @@ import ActionContext from "./ActionContext";
 const ActionProvider = ({ children }) => {
   const text = "text";
   const textTest = "Texto de prueba";
+
   return (
     <ActionContext.Provider value={[text, textTest]}>
       {children}
